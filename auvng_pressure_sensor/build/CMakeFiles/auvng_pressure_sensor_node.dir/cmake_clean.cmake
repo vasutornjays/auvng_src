454f569edea231d99956a7c93bac73504f1f310e
vasutornjays <vasutorn.s@ku.th>
2019-02-15 10:54:33 +0700

    Test simple code and install ftd2xx lib

---
+++ auvng_pressure_sensor/build/CMakeFiles/auvng_pressure_sensor_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/auvng_pressure_sensor_node.dir/src/um232_master_i2c.cpp.o"
+  "devel/lib/auvng_pressure_sensor/auvng_pressure_sensor_node.pdb"
+  "devel/lib/auvng_pressure_sensor/auvng_pressure_sensor_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/auvng_pressure_sensor_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
