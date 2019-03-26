6b5f26b04c362c1f04901cb0762b900c8d479ae3
vasutornjays <vasutorn.s@ku.th>
2019-03-26 13:28:04 +0000

    finish torque to pwn

--- auvng_thruster/src/thrust_2_pwm_node.cpp
+++ auvng_thruster/src/thrust_2_pwm_node.cpp
@@ -1,11 +1,36 @@
 #include <ros/ros.h>
 #include <hg_ros_pololu/Pwm.h>
+#include <auvng_thruster/thruster_torque.h>
+#include <math.h>
+
+hg_ros_pololu::Pwm pwm;
 
 ros::Publisher pub;
 
+int torque_to_pwm(double t){
+    if(t < -0.0272155){
+        return int((((0.003687225246674*pow(t,3)) + (0.027256336314212*pow(t,2))
+           +(0.138696210404033*t) + 1.466875275577851)) * 1000);
+    } else if (t > 0.0589670){
+        return int((((0.001239041850837*pow(t,3)) + (-0.013249188582324*pow(t,2))
+           +(0.108242941336985*t) + 1.532708016436602)) * 1000);
+    } else {
+        return 1500;
+    }
+}
 
-// msg.header.stamp = ros::Time::now();
+void callback(auvng_thruster::thruster_torque t)
+{
+    pwm.pwm = {1500,15000,1500,1500,1500,1500,1500,1500};
 
+    for(int i=0; i<8;i++){
+        pwm.pwm[i] = torque_to_pwm(t.thruster_torque[i]);
+    }
+    
+
+    pub.publish(pwm);
+    ROS_INFO("pwm[0]: [%d]", pwm.pwm[0]);
+}
 
 
 int main(int argc, char **argv)
@@ -14,23 +39,10 @@ int main(int argc, char **argv)
     ros::NodeHandle n;
 
     pub = n.advertise<hg_ros_pololu::Pwm>("pwm", 100);
+    ros::Subscriber sub = n.subscribe("thruster_torque", 1000, callback);
 
-    hg_ros_pololu::Pwm pwm;
-
-    ros::Rate loop_rate(10);
-
-    while (ros::ok())
-    {
-        pwm.pwm = {1500,15000,1500,1500,1500,1500,1500,1500};
-
-        pub.publish(pwm);
-
-        ROS_INFO("Spinning node");
-
-        ros::spinOnce();
-
-        loop_rate.sleep();
-    }
+    ROS_INFO("Spinning node");
+    ros::spin();
     
 
     return 0;
